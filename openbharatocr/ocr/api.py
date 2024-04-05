a1f44255e68f47de80d29344477b3585d1b7686f
kshipra108 <kshipra@essentia.email>
2024-04-05 08:47:56 +0530

    Include passport function to necessary files

--- openbharatocr/ocr/api.py
+++ openbharatocr/ocr/api.py
@@ -1,3 +1,4 @@
 from openbharatocr.ocr.pan import pan
 from openbharatocr.ocr.aadhaar import front_aadhaar, back_aadhaar
 from openbharatocr.ocr.driving_licence import driving_licence
+from openbharatocr.ocr.passport import passport

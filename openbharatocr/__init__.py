3d850b10a9f93b0bd23db69ac31aa290f8b36bb0
kshipra108 <kshipra@essentia.email>
2024-04-05 08:51:08 +0530

    Pass Black check

--- openbharatocr/__init__.py
+++ openbharatocr/__init__.py
@@ -2,7 +2,13 @@
 # website:   http://www.essentia.dev
 
 # import the required packages
-from openbharatocr.ocr.api import pan, front_aadhaar, back_aadhaar, driving_licence, passport
+from openbharatocr.ocr.api import (
+    pan,
+    front_aadhaar,
+    back_aadhaar,
+    driving_licence,
+    passport,
+)
 
 
 __all__ = ["pan", "front_aadhaar", "back_aadhaar", "driving_licence", "passport"]

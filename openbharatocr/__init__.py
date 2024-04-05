62c153b098640a8fd27766cce493414409f6f2aa
kshipra108 <kshipra@essentia.email>
2024-04-05 08:47:56 +0530

    Include passport function to necessary files

--- openbharatocr/__init__.py
+++ openbharatocr/__init__.py
@@ -2,7 +2,7 @@
 # website:   http://www.essentia.dev
 
 # import the required packages
-from openbharatocr.ocr.api import pan, front_aadhaar, back_aadhaar, driving_licence
+from openbharatocr.ocr.api import pan, front_aadhaar, back_aadhaar, driving_licence, passport
 
 
-__all__ = ["pan", "front_aadhaar", "back_aadhaar", "driving_licence"]
+__all__ = ["pan", "front_aadhaar", "back_aadhaar", "driving_licence", "passport"]

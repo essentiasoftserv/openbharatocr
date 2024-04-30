dd55bc2bdb4ce7e0e3a1d98fe26f790a9ce4e8fa
kunal kumar kushwaha <kunalkumar.kus@gmail.com>
2024-04-30 14:09:36 +0530

    add new Voter ID feature

--- openbharatocr/ocr/api.py
+++ openbharatocr/ocr/api.py
@@ -2,3 +2,4 @@ from openbharatocr.ocr.pan import pan
 from openbharatocr.ocr.aadhaar import front_aadhaar, back_aadhaar
 from openbharatocr.ocr.driving_licence import driving_licence
 from openbharatocr.ocr.passport import passport
+from openbharatocr.ocr.voter_id import voter_id_front, voter_id_back

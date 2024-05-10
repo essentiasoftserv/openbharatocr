3445118415834282967b9f702d4726535a36f3c4
kshipra108 <kshipra@essentia.email>
2024-05-10 08:59:53 +0530

    Add logic for water bill OCR

--- openbharatocr/ocr/api.py
+++ openbharatocr/ocr/api.py
@@ -4,3 +4,4 @@ from openbharatocr.ocr.driving_licence import driving_licence
 from openbharatocr.ocr.passport import passport
 from openbharatocr.ocr.voter_id import voter_id_front, voter_id_back
 from openbharatocr.ocr.vehicle_registration import vehicle_registration
+from openbharatocr.ocr.water_bill import water_bill

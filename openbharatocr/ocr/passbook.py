a903d68955bc7159409ce9b5756b4533a58a68aa
Rishabh0411 <rishabh.sharma041108@gmail.com>
2024-05-27 11:11:56 +0530

    black_check resolved

--- openbharatocr/ocr/passbook.py
+++ openbharatocr/ocr/passbook.py
@@ -11,6 +11,7 @@ def extract_name(input):
     else:
         return None
 
+
 def extract_cif_no(input):
     regex = re.compile(r"CIF\s*:\s*(\d+)", re.IGNORECASE)
     match = re.search(regex, input)
@@ -18,6 +19,7 @@ def extract_cif_no(input):
         return match.group(1)
     else:
         return None
+
 
 def extract_account_no(input):
     regex = re.compile(r"Account\s*No\s*:\s*(\d+)", re.IGNORECASE)
@@ -27,6 +29,7 @@ def extract_account_no(input):
     else:
         return None
 
+
 def extract_address(input):
     regex = re.compile(r"Address\s*:\s*(.*)", re.IGNORECASE)
     match = regex.search(input)
@@ -34,6 +37,7 @@ def extract_address(input):
         return match.group(1).strip()
     else:
         return None
+
 
 def extract_phone(input):
     regex = re.compile(r"Phone\s*:\s*(.*)", re.IGNORECASE)
@@ -43,6 +47,7 @@ def extract_phone(input):
     else:
         return None
 
+
 def extract_email(input):
     regex = re.compile(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b")
     match = re.search(regex, input)
@@ -50,7 +55,8 @@ def extract_email(input):
         return match.group(0)
     else:
         return None
-    
+
+
 def extract_nomination_name(input):
     regex = re.compile(r"Nomination\s*:\s*(.*)", re.IGNORECASE)
     match = re.search(regex, input)
@@ -58,6 +64,7 @@ def extract_nomination_name(input):
         return match.group(1).strip()
     else:
         return None
+
 
 def extract_branch_name(input):
     regex = re.compile(r"Branch\s*:\s*(.*)", re.IGNORECASE)
@@ -67,6 +74,7 @@ def extract_branch_name(input):
     else:
         return None
 
+
 def extract_bank_name(input):
     regex = re.compile(r"Bank\s*:\s*(.*)", re.IGNORECASE)
     match = re.search(regex, input)
@@ -75,6 +83,7 @@ def extract_bank_name(input):
     else:
         return None
 
+
 def extract_date_of_issue(input):
     regex = re.compile(r"Date\s*of\s*Issue\s*:\s*(\d{2}/\d{2}/\d{4})", re.IGNORECASE)
     match = re.search(regex, input)
@@ -82,6 +91,7 @@ def extract_date_of_issue(input):
         return match.group(1)
     else:
         return None
+
 
 def parse_passbook_frontpage(image_path):
     image = Image.open(image_path)
@@ -97,7 +107,7 @@ def parse_passbook_frontpage(image_path):
         "nomination_name": extract_nomination_name(extracted_text),
         "branch_name": extract_branch_name(extracted_text),
         "bank_name": extract_bank_name(extracted_text),
-        "date_of_issue": extract_date_of_issue(extracted_text)
+        "date_of_issue": extract_date_of_issue(extracted_text),
     }
 
     return passbook_info

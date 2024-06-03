9443dfaee215156418cbf41be1b665b1934015ad
Rishabh0411 <rishabh.sharma041108@gmail.com>
2024-06-03 12:47:41 +0530

    passbook logic (eayOCR)

--- openbharatocr/ocr/passbook.py
+++ openbharatocr/ocr/passbook.py
@@ -1,28 +1,28 @@
-import pytesseract
-from PIL import Image
+import cv2
+import easyocr
 import re
 
 
 def extract_name(input):
-    regex = re.compile(r'Customer Name\s+([A-Z\s]+)\s+A/C')
+    regex = re.compile(r"Customer Name\s+([A-Z\s]+)")
     match = re.search(regex, input)
     if match:
         return match.group(1).strip()
-    else:
-        return None
+    return None
 
 
 def extract_open_date(input):
-    regex = re.compile(r'(?:AIC|A/C) Open Date:\s*(\d{2}\s\w{3}\s\d{4})')
+    regex = re.compile(r"Open Date\s*(\d{1,2} \w{3} \d{4})")
     match = re.search(regex, input)
     if match:
         return match.group(1)
-    else:
-        return None
+    return None
 
 
 def extract_bank_name(input):
-    regex = re.compile(r'^[A-Z\s]+BANK\sLTD\.', re.MULTILINE)
+    regex = re.compile(
+        r"\b[A-Za-z\s&]+(?:BANK|BANK LTD|BANK LIMITED|CREDIT UNION)\b", re.MULTILINE
+    )
     match = re.search(regex, input)
     if match:
         return match.group(0).strip()
@@ -30,32 +30,36 @@ def extract_bank_name(input):
         return None
 
 
-
-def extract_branch_name(input):
-    regex = re.compile(r"Branch Name\s*:\s*(.+)", re.IGNORECASE)
+def extract_phone(input):
+    regex = re.compile(r"Mobile No\s*(\d+)", re.IGNORECASE)
     match = re.search(regex, input)
     if match:
         return match.group(1).strip()
-    else:
-        return None
+    return None
+
+
+def extract_branch_name(input):
+    regex = re.compile(r"Branch Name\s*([A-Za-z\d\s-]+)", re.IGNORECASE)
+    match = re.search(regex, input)
+    if match:
+        return match.group(1).strip()
+    return None
 
 
 def extract_nomination_name(input):
-    regex = re.compile(r'Nomina(?:non|tion)\s+([A-Z][a-z]+\s[A-Z][a-z]+)')
+    regex = re.compile(r"Nomina(?:non|tion)\s+([A-Z][a-z]+\s[A-Z][a-z]+)")
     match = re.search(regex, input)
     if match:
         return match.group(1).strip()
-    else:
-        return None
+    return None
 
 
 def extract_email(input):
-    regex = re.compile(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')
+    regex = re.compile(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}")
     match = re.search(regex, input)
     if match:
         return match.group(0)
-    else:
-        return None
+    return None
 
 
 def extract_account_no(input):
@@ -63,31 +67,28 @@ def extract_account_no(input):
     match = re.search(regex, input)
     if match:
         return match.group(1)
-    else:
-        return None
+    return None
 
 
 def extract_cif_no(input):
-    regex = re.compile(r'CIF No\s*[>:]\s*(\d+)')
+    regex = re.compile(r"CIF(?: No)?\.?\s*(\d+)", re.IGNORECASE)
     match = re.search(regex, input)
     if match:
         return match.group(1)
-    else:
-        return None
+    return None
 
 
 def extract_address(input):
     regex = [
-        r'\d+\s[A-Za-z\s,]+(?:Road|Street|Avenue|Boulevard|Lane|Drive|Court|Place|Square|Plaza|Terrace|Trail|Parkway|Circle)\s*,?\s*(?:\d{5}|\d{5}-\d{4})?',
-        r'\d+\s[A-Za-z\s,]+(?:Road|Street|Avenue|Boulevard|Lane|Drive|Court|Place|Square|Plaza|Terrace|Trail|Parkway|Circle)', 
-        r'\d{1,5}\s[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*\d{5}',
-        r'\d{1,5}\s[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*[A-Za-z\s]+',
-        r'\d{1,5}\s[A-Za-z\s]+,\s*[A-Za-z\s]+',
-        r'[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*\d{5}',
-        r'[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*\d{5}',
-        r'[A-Za-z\s]+,\s*\d{5}' 
+        r"\d+\s[A-Za-z\s,]+(?:Road|Street|Avenue|Boulevard|Lane|Drive|Court|Place|Square|Plaza|Terrace|Trail|Parkway|Circle)\s*,?\s*(?:\d{5}|\d{5}-\d{4})?",
+        r"\d+\s[A-Za-z\s,]+(?:Road|Street|Avenue|Boulevard|Lane|Drive|Court|Place|Square|Plaza|Terrace|Trail|Parkway|Circle)",
+        r"\d{1,5}\s[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*\d{5}",
+        r"\d{1,5}\s[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*[A-Za-z\s]+",
+        r"\d{1,5}\s[A-Za-z\s]+,\s*[A-Za-z\s]+",
+        r"[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*\d{5}",
+        r"[A-Za-z\s]+,\s*[A-Za-z\s]+,\s*\d{5}",
+        r"[A-Za-z\s]+,\s*\d{5}",
     ]
-    
     for pattern in regex:
         match = re.search(pattern, input)
         if match:
@@ -95,19 +96,18 @@ def extract_address(input):
     return None
 
 
-def extract_phone(input):
-    regex = re.compile(r"Mobile No\s*:\s*(.*)", re.IGNORECASE)
-    match = re.search(regex, input)
-    if match:
-        return match.group(1).strip()
-    else:
-        return None
-
-
 def parse_passbook_frontpage(image_path):
-    image = Image.open(image_path)
-    extracted_text = pytesseract.image_to_string(image)
-    print(extracted_text)
+    reader = easyocr.Reader(["en"])
+
+    image = cv2.imread(image_path)
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    results = reader.readtext(gray_image)
+
+    extracted_text = " ".join([text for _, text, _ in results])
+    print("-----", extracted_text)
+
     passbook_info = {
         "cif_no": extract_cif_no(extracted_text),
         "name": extract_name(extracted_text),
@@ -122,5 +122,6 @@ def parse_passbook_frontpage(image_path):
     }
 
     return passbook_info
+
 
 parse_passbook_frontpage("/home/rishabh/openbharatocr/openbharatocr/ocr/passbook1.jpeg")

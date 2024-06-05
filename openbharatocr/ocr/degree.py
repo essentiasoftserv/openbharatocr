59eea53173a891eae7d65c81966f534ce0d38466
Rishabh0411 <rishabh.sharma041108@gmail.com>
2024-06-05 10:17:15 +0530

    added degree logic

---
+++ openbharatocr/ocr/degree.py
@@ -0,0 +1,69 @@
+import cv2
+import pytesseract
+import re
+from pytesseract import Output
+
+
+def extract_name(input):
+    regex = re.compile(
+        r"has conferred upon\s+([A-Z][a-z]+\s[A-Z][a-z]+([A-Z][a-z]))|awarded to\s+([A-Z][a-z]+\s[A-Z][a-z]+(?:\s[A-Z][a-z]))",
+        re.IGNORECASE,
+    )
+    match = re.search(regex, input)
+    if match:
+        return match.group(1).strip()
+    return None
+
+
+def extract_degree_name(input):
+    regex = re.compile(
+        r"\b(?:Bachelor|Master|Doctor|Associate|B\.A\.|B\.Sc\.|M\.A\.|M\.Sc\.|Ph\.D\.|M\.B\.A\.|B\.E\.|B\.Tech|M\.E\.|M\.Tech|B\.Com|M\.Com|B\.Ed|M\.Ed|B\.Pharm|M\.Pharm|B\.Arch|M\.Arch|LL\.B|LL\.M|D\.Phil|D\.Lit|BFA|MFA|MRes|MSt)\s*(?:of\s*[A-Za-z\s]+)?\b",
+        re.IGNORECASE,
+    )
+    match = re.search(regex, input)
+    if match:
+        return match.group(0).strip()
+    return None
+
+
+def extract_institution_name(input):
+    regex = re.compile(
+        r"\b(?:[A-Za-z\s&]+(?:College|University|Institute|Academy|School|Polytechnic|Center|Centre|Faculty|Campus|School of [A-Za-z\s]+|College of [A-Za-z\s]+|Institute of [A-Za-z\s]+|University of [A-Za-z\s]+))\b",
+        re.IGNORECASE,
+    )
+    match = re.search(regex, input)
+    if match:
+        return match.group(0).strip()
+    return None
+
+
+def extract_year_of_passing(input):
+    regex = re.compile(
+        r"(?:year\s*of\s*passing|in\s*the\s*year|having\s*passed\s*the\s*examination\s*of|passed\s*in|dated)\s*[:\-]?\s*(\d{4})",
+        re.IGNORECASE,
+    )
+    match = re.search(regex, input)
+    if match:
+        return match.group(1)
+    return None
+
+
+def parse_degree_certificate(image_path):
+    
+    image = cv2.imread(image_path)
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    extracted_text = pytesseract.image_to_string(gray_image, output_type=Output.STRING)
+
+    degree_info = {
+        "Name": extract_name(extracted_text),
+        "Degree Name": extract_degree_name(extracted_text),
+        "University Name": extract_institution_name(extracted_text),
+        "Year of Passing": extract_year_of_passing(extracted_text),
+    }
+
+    return degree_info
+
+
+parse_degree_certificate("/home/rishabh/openbharatocr/openbharatocr/ocr/degree1.jpeg")

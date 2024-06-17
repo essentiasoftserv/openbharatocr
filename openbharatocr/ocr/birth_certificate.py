da3f28144daff6e506f0750b89b28b5723ceebe1
Rishabh0411 <rishabh.sharma041108@gmail.com>
2024-06-17 11:15:16 +0530

    birth certificate logic

--- openbharatocr/ocr/birth_certificate.py
+++ openbharatocr/ocr/birth_certificate.py
@@ -1,19 +1,27 @@
 import easyocr
 import re
 
+
 def extract_name(text):
-    regex = re.compile(r"Name\s*([A-Za-z\s]+)(?=\s*(?:Sex|Date\s*Birth))", re.IGNORECASE)
+    regex = re.compile(
+        r"Name\s*([A-Za-z\s]+)(?=\s*(?:Sex|Date\s*Birth))", re.IGNORECASE
+    )
     match = re.search(regex, text)
     if match:
         return match.group(1).strip()
     return None
 
+
 def extract_address_of_birth_place(text):
-    regex = re.compile(r"Place\s+of\s+Birth\s*:?[\s\n]*([A-Z\s,.-]+)(?=\s+Name\s+(?:Mcther|Mother))", re.IGNORECASE)
+    regex = re.compile(
+        r"Place\s+of\s+Birth\s*:?[\s\n]*([A-Z\s,.-]+)(?=\s+Name\s+(?:Mcther|Mother))",
+        re.IGNORECASE,
+    )
     match = re.search(regex, text)
     if match:
         return match.group(1).strip()
     return None
+
 
 def extract_dob(text):
     regex = re.compile(r"Date\s+Birth\s+(\d{2}/\d{2}/\d{4})", re.IGNORECASE)
@@ -22,12 +30,14 @@ def extract_dob(text):
         return match.group(1).strip()
     return None
 
+
 def extract_mother_name(text):
     regex = re.compile(r"Name\s+(?:Mcther|Mother)\s+([\w]+)", re.IGNORECASE)
     match = re.search(regex, text)
     if match:
         return match.group(1).strip()
     return None
+
 
 def extract_father_name(text):
     regex = re.compile(r"Name\s+the\s+Father\s+([\w]+)", re.IGNORECASE)
@@ -36,12 +46,14 @@ def extract_father_name(text):
         return match.group(1).strip()
     return None
 
+
 def extract_registration_number(text):
     regex = re.compile(r"Registration\s+No\s*([\w]+)", re.IGNORECASE)
     match = re.search(regex, text)
     if match:
         return match.group(1).strip()
     return None
+
 
 def extract_registration_date(text):
     regex = re.compile(r"Date\s+of\s+Registration\s+(\d{2}/\d{2}/\d{4})", re.IGNORECASE)
@@ -50,11 +62,12 @@ def extract_registration_date(text):
         return match.group(1).strip()
     return None
 
+
 def parse_birth_certificate(image_path):
-    reader = easyocr.Reader(['en'])  
-    result = reader.readtext(image_path, detail=0)  
-    
-    extracted_text = ' '.join(result)
+    reader = easyocr.Reader(["en"])
+    result = reader.readtext(image_path, detail=0)
+
+    extracted_text = " ".join(result)
 
     birth_certificate_info = {
         "Name": extract_name(extracted_text),
